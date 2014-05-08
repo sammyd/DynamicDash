2b9f2040c87eb25e5e6fa1a8306d2a199b98f97f
Sam Davies <sdavies@shinobicontrols.com>
2014-05-07 23:45:17 -0400

    Create a standard datasource for the 2-series charts

---
+++ DynamicDash/SCMultiAxisCategoryDataSource.h
@@ -0,0 +1,22 @@
+//
+//  SCMultiAxisCategoryDataSource.h
+//  DynamicDash
+//
+//  Created by Sam Davies on 07/05/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <ShinobiCharts/ShinobiChart.h>
+
+@interface SCMultiAxisCategoryDataSource : NSObject
+
+- (instancetype)initWithChart:(ShinobiChart *)chart categories:(NSArray *)categories;
+
+@property (nonatomic, strong, readonly) ShinobiChart *chart;
+@property (nonatomic, strong, readonly) NSArray *categories;
+@property (nonatomic, strong, readonly) NSArray *yAxes;
+
+- (void)animateToValues:(NSArray *)values;
+
+@end
---
+++ DynamicDash/SCMultiAxisCategoryDataSource.m
@@ -0,0 +1,139 @@
+//
+//  SCMultiAxisCategoryDataSource.m
+//  DynamicDash
+//
+//  Created by Sam Davies on 07/05/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+#import "SCMultiAxisCategoryDataSource.h"
+
+@interface SCMultiAxisCategoryDataSource () <SChartDatasource>
+
+@property (nonatomic, strong, readwrite) ShinobiChart *chart;
+@property (nonatomic, strong, readwrite) NSArray *categories;
+@property (nonatomic, strong, readwrite) NSArray *yAxes;
+@property (nonatomic, strong) NSArray *datapoints;
+
+@end
+
+@implementation SCMultiAxisCategoryDataSource
+
+- (instancetype)initWithChart:(ShinobiChart *)chart categories:(NSArray *)categories
+{
+    self = [super init];
+    if(self) {
+        [self prepareChart:chart];
+        self.categories = categories;
+        [self prepareDataPoints];
+    }
+    return self;
+}
+
+#pragma mark - API Methods
+- (void)animateToValues:(NSArray *)values
+{
+    [self validateValues:values];
+    [values enumerateObjectsUsingBlock:^(NSArray *dpValues, NSUInteger seriesIdx, BOOL *oStop) {
+        [dpValues enumerateObjectsUsingBlock:^(NSNumber *value, NSUInteger dataIdx, BOOL *iStop) {
+            SChartDataPoint *dp = self.datapoints[seriesIdx][dataIdx];
+            dp.yValue = value;
+        }];
+    }];
+    [self.chart reloadData];
+    [self.chart redrawChart];
+}
+
+
+#pragma mark - Non-public methods
+- (void)prepareChart:(ShinobiChart *)chart
+{
+    SChartCategoryAxis *xAxis = [SChartCategoryAxis new];
+    chart.xAxis = xAxis;
+    
+    SChartNumberAxis *firstYAxis = [SChartNumberAxis new];
+    [chart addYAxis:firstYAxis];
+    SChartNumberAxis *secondYAxis = [SChartNumberAxis new];
+    secondYAxis.axisPosition = SChartAxisPositionReverse;
+    [chart addYAxis:secondYAxis];
+    
+    chart.datasource = self;
+    
+    self.chart = chart;
+}
+
+- (void)prepareDataPoints
+{
+    // Want a 2-dimensional array. series x datapoints
+    NSMutableArray *series = [NSMutableArray array];
+    for(NSUInteger seriesIdx = 0; seriesIdx < 2; seriesIdx ++) {
+        NSMutableArray *datapoints = [NSMutableArray array];
+        [self.categories enumerateObjectsUsingBlock:^(NSString *category, NSUInteger idx, BOOL *stop) {
+            SChartDataPoint *dp = [SChartDataPoint new];
+            dp.xValue = category;
+            dp.yValue = @0;
+            [datapoints addObject:dp];
+        }];
+        [series addObject:[datapoints copy]];
+    }
+    self.datapoints = [series copy];
+}
+
+- (void)validateValues:(NSArray *)values
+{
+    if([values count] != 2) {
+        NSException *ex = [NSException exceptionWithName:NSInvalidArgumentException
+                                                  reason:@"There should be data for 2 series provided in the values array"
+                                                userInfo:nil];
+        @throw ex;
+    }
+    
+    [values enumerateObjectsUsingBlock:^(NSArray *seriesValues, NSUInteger idx, BOOL *stop) {
+        if([seriesValues count] != [self.categories count]) {
+            NSException *ex = [NSException exceptionWithName:NSInvalidArgumentException
+                                                      reason:@"Each array of series data should contain the same number values as categories"
+                                                    userInfo:nil];
+            @throw ex;
+        }
+    }];
+}
+
+
+#pragma mark - SChartDatasource methods
+- (NSInteger)numberOfSeriesInSChart:(ShinobiChart *)chart
+{
+    return 2;
+}
+
+- (SChartSeries *)sChart:(ShinobiChart *)chart seriesAtIndex:(NSInteger)index
+{
+    switch (index) {
+        case 0:
+            return [SChartColumnSeries new];
+            break;
+            
+        case 1:
+            return [SChartLineSeries new];
+            break;
+        default:
+            return nil;
+            break;
+    }
+}
+
+- (NSInteger)sChart:(ShinobiChart *)chart numberOfDataPointsForSeriesAtIndex:(NSInteger)seriesIndex
+{
+    return [self.datapoints[seriesIndex] count];
+}
+
+- (id<SChartData>)sChart:(ShinobiChart *)chart dataPointAtIndex:(NSInteger)dataIndex forSeriesAtIndex:(NSInteger)seriesIndex
+{
+    return self.datapoints[seriesIndex][dataIndex];
+}
+
+- (SChartAxis *)sChart:(ShinobiChart *)chart yAxisForSeriesAtIndex:(NSInteger)index
+{
+    return self.yAxes[index];
+}
+
+@end
